f5f5d794170913898d7e8bcc99346bf57de4aa96
joyalshaji135 <joyalshaji135@gmail.com>
2023-08-22 07:36:28 -0700

    It's not completed

--- program45.c
+++ program45.c
@@ -7,4 +7,12 @@ int main()
 {
     int arr[100];
     int i,limit;
+    printf("Enter the Limit of an Array :");
+    scanf("%d",&limit);
+    printf("Enter the Elements of an Array :");
+    for(i=0; i<limit; i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    PrimeAndNonPrime(limit,arr);
 }
