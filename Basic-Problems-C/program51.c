a5e463bdf157bfd67d1e33ad8d231332166c1711
joyalshaji135 <joyalshaji135@gmail.com>
2023-08-24 09:35:59 -0700

    24-08-2023

--- Basic-Problems-C/program51.c
+++ Basic-Problems-C/program51.c
@@ -1,2 +1,11 @@
 // Write a program to count the total number of negative elements in an array.
 
+#include<stdio.h>
+void CountNegative(int limit,int arr[]);
+int main()
+{
+    int arr[100];
+    int i,limit;
+    return 0;
+}
+
